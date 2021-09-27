8bd145555a44c3b5144495f2d22da050ffef9cc0
Julian Bachmann <julian.bachmann@roche.com>
2021-09-27 09:11:17 +0200

    new structure, multiple solutions

---
+++ Kompetenzen/JSONSerializer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace JSONSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Directory.GetCurrentDirectory().ToString() + "BinaryFile.bin";
+            IFormatter binaryFormatterObject = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+
+            }
+        }
+    }
+}
